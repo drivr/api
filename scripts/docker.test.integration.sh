1322edd50db8a4f7bee9d75052f41c02079336f6
Renan Gomes <renan.gomes@live.com>
2021-02-12 08:34:24 -0300

    Added a sleep after the creation of Postgres container.

--- scripts/docker.test.integration.sh
+++ scripts/docker.test.integration.sh
@@ -1,6 +1,10 @@
 DOCKER_CONTAINER_ID=$(sh ./scripts/docker.db.testing.sh)
 echo "Created docker container for integration tests: $DOCKER_CONTAINER_ID"
 
+# Wait a little for postgres connection.
+sleep 2s
+
+# Run the integration tests.
 poetry run behave
 
 echo "Removing container for integration tests: $DOCKER_CONTAINER_ID"
