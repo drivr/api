0177c4a5bc5995ff7a337953efa9d23adccb77bb
RN4N <3982052+rn4n@users.noreply.github.com>
2021-02-17 04:53:21 -0300

    Add quality to integration tests (#4)
    * Add the 'integration' folder to linter/quality analysis.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,9 @@ APP_PATH=drivr
 # The unit tests path.
 UNIT_TEST_PATH=tests/unit
 
+# The integration tests path.
+INTEGRATION_TEST_PATH=tests/integration
+
 ## =================== JOBS =================== ##
 
 # Remove unsed variables and imports.
@@ -11,15 +14,15 @@ poetry run autoflake \
     --remove-all-unused-imports \
     --recursive \
     --remove-unused-variables \
-    --in-place $APP_PATH $UNIT_TEST_PATH
+    --in-place $APP_PATH $UNIT_TEST_PATH $INTEGRATION_TEST_PATH
 
 # Sort imports from app and unit tests.
 # There's a configuration on 'pyproject.toml' to make isort compatible with black.
 # See: https://black.readthedocs.io/en/stable/compatible_configs.html#isort
-poetry run isort $APP_PATH $UNIT_TEST_PATH
+poetry run isort $APP_PATH $UNIT_TEST_PATH $INTEGRATION_TEST_PATH
 
 # Audit python files on app and unit tests.
-poetry run pylama $APP_PATH $UNIT_TEST_PATH
+poetry run pylama $APP_PATH $UNIT_TEST_PATH $INTEGRATION_TEST_PATH
 
 # Pydocstyle:
 #   - D101: Missing docstring in public class.
@@ -30,4 +33,4 @@ poetry run pydocstyle \
     --match-dir="^(?!migrations).*" \
     $APP_PATH
 
-poetry run black --check $APP_PATH $UNIT_TEST_PATH
+poetry run black --check $APP_PATH $UNIT_TEST_PATH $INTEGRATION_TEST_PATH
