53e6d1a014b7870033a199527d401c734578be04
Renan Gomes <renan.gomes@live.com>
2021-02-17 03:16:06 -0300

    Added specific methods for create/update users.

--- drivr/crud/crud_users.py
+++ drivr/crud/crud_users.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import Any, Dict, Optional, Union
 
 from sqlalchemy.orm.session import Session
 
 from drivr import model, schema, security
+from drivr.security.password import hash_password
 
 from .crud_base import CRUDBase
 
@@ -54,6 +55,57 @@ class CRUDUsers(
             otherwise `None` is returned.
         """
         return db.query(model.User).filter_by(email=email).first()
+
+    def create(
+        self,
+        db: Session,
+        schema: schema.UserCreate,
+    ) -> Optional[model.User]:
+        """
+        Persist a new user.
+
+        Args:
+            db: the database session.
+            schema: the schema for create the new user.
+
+        Returns:
+            The created user.
+        """
+        user = model.User(**schema.dict())
+        user.password = hash_password(user.password.get_secret_value())
+
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+        return user
+
+    def update(
+        self,
+        db: Session,
+        user: model.User,
+        schema: schema.UserUpdate,
+    ) -> model.User:
+        """
+        Update an existing user.
+
+        Args:
+            - db: the database session.
+            - entity: the entity to be updated.
+            - schema: the data used to update the user entity.
+
+        Returns:
+            - the updated user entity.
+        """
+
+        update_data = schema.dict(exclude_unset=True)
+
+        if schema.password:
+            hashed_password = hash_password(schema.password.get_secret_value())
+            del update_data["password"]
+            update_data["password"] = hashed_password
+
+        return super().update(db=db, model=user, schema=update_data)
 
 
 users = CRUDUsers(model=model.User)
--- tests/unit/crud/test_users.py
+++ tests/unit/crud/test_users.py
@@ -1,4 +1,6 @@
-from drivr import model
+from pydantic.types import SecretStr
+
+from drivr import model, schema
 from drivr.crud.crud_users import CRUDUsers
 from tests.unit.factories import UserFactory
 
@@ -82,7 +84,7 @@ class TestAuthenticate:
             hashed_password=user.password,
         )
 
-    def test_should_return_the_user_object_when_it_exist_and_password_match(  # noqa
+    def test_should_return_the_user_object_when_it_exist_and_password_match(
         self,
         faker,
         mocker,
@@ -115,4 +117,110 @@ class TestAuthenticate:
         verify_password.assert_called_once_with(
             plain_text=password,
             hashed_password=user.password,
+        )
+
+
+class TestCreate:
+    def test_should_commit_and_refresh_the_create_entity(self, faker, mocker):
+        hashed_password = faker.sha1()
+
+        db = mocker.MagicMock()
+        created_user = UserFactory(password=SecretStr(hashed_password))
+
+        mocker.patch(f"{MODULE}.model.User", return_value=created_user)
+        hash_password = mocker.patch(
+            f"{MODULE}.hash_password",
+            return_value=hashed_password,
+        )
+
+        user_schema = schema.UserCreate(
+            email=faker.email(), password=faker.word()
+        )
+
+        actual_user = CRUDUsers(model=model.User).create(
+            db=db, schema=user_schema
+        )
+
+        assert actual_user == created_user
+        hash_password.assert_called_once_with(created_user.password)
+        db.add.assert_called_once_with(created_user)
+        db.commit.assert_called_once()
+        db.refresh.assert_called_once_with(created_user)
+
+
+class TestUpdate:
+    def test_should_update_the_user_with_the_schema_data_provided(
+        self,
+        mocker,
+    ):
+        db = mocker.MagicMock()
+        user = UserFactory()
+
+        hash_password = mocker.patch(f"{MODULE}.hash_password")
+        update_method = mocker.patch(
+            f"{MODULE}.CRUDBase.update",
+            return_value=user,
+        )
+
+        user_schema = mocker.MagicMock()
+        user_schema.password = None
+
+        actual_user = CRUDUsers(model=model.User).update(
+            db=db,
+            user=user,
+            schema=user_schema,
+        )
+
+        assert actual_user == user
+
+        update_method.assert_called_once_with(
+            db=db,
+            model=user,
+            schema=user_schema.dict(exclude_unset=True),
+        )
+        hash_password.assert_not_called()
+
+    def test_should_hash_the_new_password_when_it_is_present_in_schema_data(
+        self,
+        faker,
+        mocker,
+    ):
+        plain_password = faker.word()
+        hashed_password = faker.sha1()
+        db = mocker.MagicMock()
+        user = UserFactory()
+
+        hash_password = mocker.patch(
+            f"{MODULE}.hash_password",
+            return_value=hashed_password,
+        )
+        update_method = mocker.patch(
+            f"{MODULE}.CRUDBase.update",
+            return_value=user,
+        )
+
+        user_schema = mocker.MagicMock()
+        user_schema.password.get_secret_value.return_value = plain_password
+        user_schema.dict.return_value = {
+            "email": user_schema.email,
+            "password": hashed_password,
+        }
+
+        actual_user = CRUDUsers(model=model.User).update(
+            db=db,
+            user=user,
+            schema=user_schema,
+        )
+
+        assert actual_user == user
+
+        user_schema.password.get_secret_value.assert_called_once()
+        hash_password.assert_called_once_with(plain_password)
+        update_method.assert_called_once_with(
+            db=db,
+            model=user,
+            schema={
+                "email": user_schema.email,
+                "password": hashed_password,
+            },
         )
